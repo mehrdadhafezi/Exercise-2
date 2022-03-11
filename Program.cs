8c18d0603c19bbd4882e58a2dcbcc8fadce1e814
mehrdadhafezi <mhafezi98@gmail.com>
2022-03-11 14:11:22 +0330

    hh

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
-﻿do
-{
-    int score
-  System.Console.WriteLine("Enter your score"); 
-  score = Convert.ToInt32(Console.ReadLine());
+﻿// do
+// {
+//     int score
+//   System.Console.WriteLine("Enter your score"); 
+//   score = Convert.ToInt32(Console.ReadLine());
 
-} while (Score<10);
+// } while (score<10);
+
+int number;
+double sum = 0;
+int counter = 0;
+int avg = 0;
+do
+{
+    System.Console.WriteLine("adad bede");
+    number = Convert.ToInt32(Console.ReadLine());
+    sum = sum + number;
+    counter++;
+    avg = avg = number;
+    System.Console.WriteLine(sum / counter);
+} while (number <= 20 && number != 0);
+
+
+
